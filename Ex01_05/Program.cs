3545a32c9800a81789427206d92c9f14f972b265
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-03-31 18:34:42 +0300

    create  getNumberAndCheckStatistics method

--- Ex01_05/Program.cs
+++ Ex01_05/Program.cs
@@ -7,13 +7,52 @@ namespace Ex01_05
     {
         public static void Main()
         {
-            PrintDiamondRhombus(9);
+            GetNumberAndCheckStatistics();
         }
     }
 
-    public static void GetNumberAndCheckStatistics()
+    private static void getNumberAndCheckStatistics()
     {
+        System.Console.Write("Please enter a integer number to run statistics and press enter:");
+        string stringNumberRaw = System.Console.ReadLine();
+        int digitsAverage = 0;
+        int smallestDigit = -1;
+        int countDivideIn3 = 0;
+        int countBiggerThenUnitPlace = 0;
+        int sumAllDigit = 0;
+        bool isSuccessParseString = int.TryParse(stringNumberRaw, out int o_numberToCheck);
+        while (!isSuccessParseString)
+        {
+            System.Console.Write("invalid integer number,try again and press enter:");
+            stringNumberRaw = System.Console.ReadLine();
+            isSuccessParseString = int.TryParse(stringNumberRaw, out o_numberToCheck);
+        }
 
+        int digitAmount = stringNumberRaw.Length;
+        string unitDigitRaw = stringNumberRaw.Substring(stringNumberRaw.Length - 1);
+        int.TryParse(unitDigitRaw, out int unitDigit);
+        for(int i = 0; i < digitAmount; i++)
+        { 
+            string currentDigitString= stringNumberRaw[i].ToString();
+            int.TryParse(currentDigitString, out int currentDigit);
+           sumAllDigit += currentDigit;
+           if(currentDigit % 3 == 0)
+           {
+               countDivideIn3++;
+           }
+           if(smallestDigit < currentDigit)
+           {
+               smallestDigit = currentDigit;
+           }
+           if(currentDigit > unitDigit)
+           {
+               countBiggerThenUnitPlace++;
+           }
+        }
+
+        System.Console.WriteLine(
+            @"Number Statistic:
+            number of digit");
 
     }
 }
