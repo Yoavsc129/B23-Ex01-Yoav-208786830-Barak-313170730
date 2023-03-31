62f97b9883d67d657d6185b531f38c507ce5adc1
barakg43 <93736137+barakg43@users.noreply.github.com>
2023-03-31 17:10:03 +0300

    add program class

---
+++ Ex01_05/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Ex01_05
+{
+    class Program
+    {
+        public static void Main()
+        {
+            PrintDiamondRhombus(9);
+        }
+    }
+
+    public static void GetNumberAndCheckStatistics()
+    {
+
+
+    }
+}
